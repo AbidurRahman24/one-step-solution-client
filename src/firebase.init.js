88adb60ec8234eba57321bcfe7dab1aa1386f5c6
Abidur Niloy <niloyabidur@gmail.com>
2022-09-12 04:14:40 +0600

    firebase add

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7KPG5O5P95uev0mRWn8IYrBTYw0Siokw",
+  authDomain: "one-step-solution-e1d86.firebaseapp.com",
+  projectId: "one-step-solution-e1d86",
+  storageBucket: "one-step-solution-e1d86.appspot.com",
+  messagingSenderId: "744099451621",
+  appId: "1:744099451621:web:5539f66cccb1411f6a3a0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
