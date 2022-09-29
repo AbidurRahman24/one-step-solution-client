1ce3f0c53ff1e73a2e8ff68bafe856873b3993b7
Abidur Niloy <niloyabidur@gmail.com>
2022-09-29 10:56:41 +0600

    navigation and footer and banner done

--- src/pages/home/header/Banner.jsx
+++ src/pages/home/header/Banner.jsx
@@ -1,96 +1,67 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import Carousel from 'react-bootstrap/Carousel';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-// import "./styles.css";
 
 const Banner = () => {
-    const items = [
-        {
-          _id: 1,
-          subtitle: "Discount Up To 40% Off",
-          headtitle: "WHEELS & TIRES ",
-          headsubtitle: "COLLECTIONS",
-          btntext: "Shop Now",
-          img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
-        },
-        {
-          _id: 2,
-          subtitle: "NEW TECHNOLOGY & BUILD",
-          headtitle: "WHEELS & TIRES",
-          headsubtitle: "COLLECTIONS",
-          btntext: "Shop Now",
-          img: "https://i.ibb.co/6B35QM1/slider-2.webp",
-        },
-        {
-          _id: 3,
-          subtitle: "Discount Up To 40% Off",
-          headtitle: "WHEELS & TIRES ",
-          headsubtitle: "COLLECTIONS",
-          btntext: "Shop Now",
-          img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
-        },
-        {
-          _id: 4,
-          subtitle: "NEW TECHNOLOGY & BUILD",
-          headtitle: "WHEELS & TIRES",
-          headsubtitle: "COLLECTIONS",
-          btntext: "Shop Now",
-          img: "https://i.ibb.co/6B35QM1/slider-2.webp",
-        },
-        {
-          _id: 5,
-          subtitle: "Discount Up To 40% Off",
-          headtitle: "WHEELS & TIRES ",
-          headsubtitle: "COLLECTIONS",
-          btntext: "Shop Now",
-          img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
-        },
-      ];
-    return (
-        <Swiper
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-    >
+  const items = [
+    {
+      _id: 1,
+      subtitle: "Discount Up To 40% Off",
+      headtitle: "WHEELS & TIRES ",
+      headsubtitle: "COLLECTIONS",
+      btntext: "Shop Now",
+      img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
+    },
+    {
+      _id: 2,
+      subtitle: "NEW TECHNOLOGY & BUILD",
+      headtitle: "WHEELS & TIRES",
+      headsubtitle: "COLLECTIONS",
+      btntext: "Shop Now",
+      img: "https://i.ibb.co/6B35QM1/slider-2.webp",
+    },
+    {
+      _id: 3,
+      subtitle: "Discount Up To 40% Off",
+      headtitle: "WHEELS & TIRES ",
+      headsubtitle: "COLLECTIONS",
+      btntext: "Shop Now",
+      img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
+    },
+    {
+      _id: 4,
+      subtitle: "NEW TECHNOLOGY & BUILD",
+      headtitle: "WHEELS & TIRES",
+      headsubtitle: "COLLECTIONS",
+      btntext: "Shop Now",
+      img: "https://i.ibb.co/6B35QM1/slider-2.webp",
+    },
+    {
+      _id: 5,
+      subtitle: "Discount Up To 40% Off",
+      headtitle: "WHEELS & TIRES ",
+      headsubtitle: "COLLECTIONS",
+      btntext: "Shop Now",
+      img: "https://i.ibb.co/qdpvqqy/slider-1.webp",
+    },
+  ];
+  return (
+   
+    <Carousel >
       {items.map((item) => (
-          <SwiperSlide key={item._id}>
-            <div className="relative ">
-              <div>
-                <img src={item.img} alt="" />
-              </div>
-              <div className="absolute">
-                <h2 >
-                  {item.subtitle}
-                </h2>
-                <h1 >
-                  {item.headtitle}
-                  <br />
-                  {item.headsubtitle}
-                </h1>
-                <Link to={"/shop"}>
-                  <Button>{item.btntext}</Button>
-                </Link>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      <div className="prev hidden sm:block text-center text-neutral leading-relaxed sm:leading-10 text-xl sm:text-2xl absolute top-1/2 left-4 -translate-y-1/2 z-10 rounded-full w-8 sm:w-10 md:w-12 h-8 sm:h-10 md:h-12 border-2 border-neutral cursor-pointer hover:ring hover:ring-offset-2 duration-500 ring-neutral">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </div>
-        <div className="next  hidden sm:block text-center text-neutral leading-relaxed sm:leading-10 text-xl sm:text-2xl absolute top-1/2 right-4 -translate-y-1/2 z-10 rounded-full w-8 sm:w-10 md:w-12 h-8 sm:h-10 md:h-12 border-2 border-neutral cursor-pointer hover:ring hover:ring-offset-2 duration-500 ring-neutral">
-          <FontAwesomeIcon icon={faArrowRight} />
-        </div>
-    </Swiper>
-    );
+      <Carousel.Item key={item._id}>
+        <img
+          className="d-block w-100"
+          src={item.img}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>{item.subtitle}</h5>
+          <p>{item.headsubtitle}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      ))}
+      </Carousel>
+  );
 };
 
 export default Banner;
--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+AOS.init();
 const Footer = () => {
     return (
-        <div>
-            <p>Hello footer</p>
-        </div>
+        <footer data-aos-duration="5000">
+            <Container bg='Dark' >
+                <Row style={{ padding: '10px 10px' }}>
+                    <Col sm>
+                        <p>Hello</p>
+                    </Col>
+                    <Col sm><span className="footer-title">Menu</span>
+                        <li><Link to={"/"} className="link link-hover">Home</Link></li>
+                        <li><Link to={"/shop"} className="link link-hover">Shop</Link></li>
+                        <li ><a className="link link-hover">About</a></li>
+                        <li><a className="link link-hover">Contact Us</a></li>
+                    </Col>
+                    <Col sm><span className="footer-title">Company</span>
+                        <br />
+                        <a className="link link-hover">About us</a>
+                        <br />
+                        <a className="link link-hover">Contact</a>
+                        <br />
+                        <a className="link link-hover">Jobs</a>
+                        <br />
+                        <a className="link link-hover">Press kit</a></Col>
+                    <Col sm><span className="footer-title">Legal</span>
+                        <br />
+                        <a className="link link-hover">Terms of use</a>
+                        <br />
+                        <a className="link link-hover">Privacy policy</a>
+                        <br />
+                        <a className="link link-hover">Cookie policy</a></Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
 
