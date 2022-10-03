cb372a21a7d6f55dec77b7ef4291e64e35032707
Abidur Niloy <niloyabidur@gmail.com>
2022-10-03 09:59:33 +0600

    card done

--- src/pages/home/home/service/Service.jsx
+++ src/pages/home/home/service/Service.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import { Card, Col, ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/esm/Button';
 import { Link } from 'react-router-dom';
 
 const Service = ({handleAddProudct, product}) => {
     // console.log(product);
    const {_id, Name, image,max, description, price} = product
+   
     return (
-        <div>
-            <h1>{Name}</h1>
-            <p>{description}</p>
-            <p>{price}</p>
-            <p>{max}</p>
-            <img src={image} alt="" />
-            <Link to={`/product/${_id}`}>
+        <Col style={{margin:"10px"}} className=''>
+            <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{Name}</Card.Title>
+        <Card.Text>{description.slice(0, 99)}...</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Stock: {max}</ListGroup.Item>
+        <ListGroup.Item>Price: {price}</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link>
+        <Link to={`/product/${_id}`}>
                 <Button onClick={() => handleAddProudct(product)} variant="secondary">Order Now</Button>
             </Link>
-        </div>
+        </Card.Link>
+      </Card.Body>
+    </Card>
+            
+        </Col>
     );
 };
 
--- src/pages/home/services/HomeServices.jsx
+++ src/pages/home/services/HomeServices.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import useServices from '../../../hooks/useServices';
 import Service from '../home/service/Service';
-
+import './HomeServices.css'
 const HomeServices = () => {
     const [card, setCard] = useState([])
     const [product, setProduct] = useServices()
     // const [product, setProduct] = useState([])
-    
+
     // useEffect(() => {
     //     fetch('http://localhost:5000/order')
     //         .then(response => response.json())
@@ -16,20 +17,31 @@ const HomeServices = () => {
     //             setProduct(data)
     //         })
     // }, [])
-    const handleAddProudct =(product) =>{
+    const handleAddProudct = (product) => {
         const newCard = [...card, product]
         setCard(newCard)
-        
+
     }
     return (
         <>
-            {
-                product.map(product => <Service 
-                    key={product._id} 
-                    product={product}
-                    handleAddProudct={handleAddProudct}
-                    ></Service>)
-            }
+            <div className="services-container">
+                <Container >
+                    <div className="services-header">
+                        <h1>OUR SERVICES</h1>
+                    </div>
+                    <Row>
+                    
+                    {
+                        product.map(product => <Service
+                            key={product._id}
+                            product={product}
+                            handleAddProudct={handleAddProudct}
+                        ></Service>)
+                    }
+                   
+                    </Row>
+                    </Container>
+                    </div>
         </>
     );
 };
